9ebf76277142c616ed1179f6937e68789d5f5db8
Dharmakh <rahuldwivedikh@gmail.com>
2019-07-05 12:20:09 +0530

    changed updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/testApi", (req, res) => {
-	res.send({ Hi: "There!" });
+	res.send({ "Auther":"Dharmendra Express Js Text API" });
 });
 
 const PORT = process.env.PORT || 5000;
